ce6e23252853318b33f83ddb5335fddeb43409d6
EdYashida <107697323+EdYashida@users.noreply.github.com>
2023-01-28 14:35:03 -0300

    Create input.java

---
+++ Exemplo AP/input.java
@@ -0,0 +1,14 @@
+ab_ba.
+aab_baa.
+abb_bab.
+abbb_bbba.
+abba_abaa.
+ab_aa_bb.aab_a
+aa_a.
+ababba_abbaba.
+abc_cba.
+aa_aa.
+bb_bb.
+cc_cc.
+  
+nesse input os "_"  simbolziam movimento vazio  e os "." simbolizam o o final da entrada 
